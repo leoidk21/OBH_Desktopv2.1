5c3a85ec927a6f12445a59bb7435b586881427cf
gnawingterror <chavezleo518@gmail.com>
2025-09-10 17:39:11 +0800

    sample

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let win
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'src/preload.js')
+    }
+  })
+
+  win.loadFile('src/renderer/index.html')
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed (except on macOS)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// Re-create a window when the app is activated (macOS behavior)
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
